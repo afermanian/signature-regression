4f0997296309890e172ca696ac86f79c83a152dd
afermanian <adelinefermanian@hotmail.fr>
2020-01-17 14:44:11 +0100

    Update script convergence hatm to save  simulation parameters and results, comment estimation and simulation functions

--- script_cvg_hatm_Y_sig.py
+++ script_cvg_hatm_Y_sig.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from read_data import temp_data_to_input_matrices 
 import seaborn as sns
 import pandas as pd
+import time
+import os
 
 sns.set()
 
@@ -11,62 +13,92 @@ sns.set()
 
 d=2
 nb_points=200
-mast=3
+mast=5
 
-max_k=11
+max_k=7
 rho=0.4
 
-Y_noise_std=30
+Y_noise_std=10
 
 est=orderEstimator(d,rho=rho)
 
 # Data for selection of alpha and Kpen
-#sim=dataSimu(nb_points,5000,d)
-#X=sim.get_X()
+sim=dataSimu(nb_points,d,mast)
+X=sim.get_X(1000)
 #plt.plot(np.transpose(X[:10,:,0]))
 #plt.show()
-#Y=sim.get_Y_sig(X,mast,Y_noise_std,plot=False)
-#alpha=est.get_alpha_ridgeCV(Y,X,1,alphas=np.linspace(10**(-9),1,num=1000),plot=True)
-alpha=0.01
-print("alpha: ",alpha)
+Y=sim.get_Y_sig(X,Y_noise_std,plot=True)
+alpha=est.get_alpha_ridgeCV(Y,X,1,alphas=np.linspace(10**(-9),10**(-1),num=1000))
+
 
 # Choose Kpen
-#K_values=np.linspace(10**(-1),5*10**(2),num=500)
+#K_values=np.linspace(10**(-5),5*10**(2),num=500)
 #hatm_values=est.slope_heuristic(K_values,X,Y,max_k,alpha)
 
-# Value obtained for mast=3, 5000 data points
-Kpen=80
+Kpen=1
+#hatm,loss=est.get_hatm(Y,X,max_k,Kpen=Kpen,alpha=alpha)
+#print("hatm : ",hatm)
 
 
-n_grid=[10,50,100,500,1000,5000]
+# Value obtained for mast=3, 5000 data points : Kpen=80
+
+
+n_grid=[10,50,100,500,1000,5000,10000]
 print(n_grid)
 nb_iterations=100
 hatm_values=np.zeros(len(n_grid)*nb_iterations)
 n_values=np.zeros(len(n_grid)*nb_iterations)
-
-max_k=8
+pred_error=np.zeros(len(n_grid)*nb_iterations)
 
 for i in range(len(n_grid)):
 	print("n=",n_grid[i])
-
+	n_values[i*nb_iterations:(i+1)*nb_iterations]=np.repeat(n_grid[i],nb_iterations)
 	for j in range(nb_iterations):
 		print("Iteration nb : ",j)
-		sim=dataSimu(nb_points,n_grid[i],d)
-		X=sim.get_X()
-		Y=sim.get_Y_sig(X,mast,Y_noise_std)
+		X=sim.get_X(n_grid[i])
+		Y=sim.get_Y_sig(X,Y_noise_std)
 
 		#if i==0 and j==0:
 		#	alpha=est.get_alpha_ridgeCV(Y,X,1)
 		#	print("alpha: ",alpha)
+
+		hatm=est.get_hatm(Y,X,max_k,Kpen=Kpen,alpha=alpha)[0]
 		
-		hatm_values[i*nb_iterations+j]=est.get_hatm(Y,X,max_k,Kpen=Kpen,alpha=alpha)[0]
-		n_values[i*nb_iterations:(i+1)*nb_iterations]=np.repeat(n_grid[i],nb_iterations)
+		hatm_values[i*nb_iterations+j]=hatm
 		print('hatm is : ',hatm_values[i*nb_iterations+j])
 
-df=pd.DataFrame({'n':n_values,'hatm':hatm_values})
-print(df)
+		X_pred=sim.get_X(n_grid[i])
+		Y_pred=sim.get_Y_sig(X,Y_noise_std)
 
-fig, ax = plt.subplots()    
+		pred_error[i*nb_iterations+j]=est.get_hatL(Y,X,hatm,alpha=alpha)/n_grid[i]
+
+
+file_name="%s_cvg_hatm" % (time.strftime("%m%d-%H%M%S"))
+
+
+df=pd.DataFrame({'n':n_values.astype(int),r"Value of $\hat{m}$":hatm_values.astype(int),'pred_error':pred_error})
+print(df)
+df.to_csv(os.path.join('results',file_name+'.csv'))
+
+
+file = open(os.path.join('results',file_name+'.txt'),"w")
+file.write("d: %s \n" % (d))
+file.write('nb_points: %s \n' % (nb_points))
+file.write('mast: %s \n' %(mast))
+file.write('max_k:%s \n' % (max_k))
+file.write('rho: %s \n' % (rho))
+file.write('Y_noise_std: %s \n' %(Y_noise_std))
+file.write('alpha: %s \n' %(alpha))
+file.write('Kpen: %s \n' % (Kpen))
+file.write('nb_iterations: %s \n' % (nb_iterations))
+file.close()
+
+g=sns.catplot(x='n', hue=r"Value of $\hat{m}$", data=df,kind="count")
+plt.xlabel(r"Sample size $n$")
+plt.savefig(os.path.join('results',file_name+'_hist_hatm'+'.png'))
+plt.show()
+
+# fig, ax = plt.subplots()    
 # sns.stripplot(df.n, df.hatm, jitter=0.1, size=8, ax=ax, linewidth=.5)
 # plt.xticks(np.arange(len(n_grid)),n_grid)
 # plt.yticks(np.arange(0,max_k+1),np.arange(0,max_k+1))
@@ -74,10 +106,18 @@ fig, ax = plt.subplots()
 # plt.ylabel(r"Estimator $\hat{m}$")
 # plt.show()
 
-sns.boxplot(x=df.n,y=df.hatm)
+sns.boxplot(x=df.n,y=df[r"Value of $\hat{m}$"])
 plt.xticks(np.arange(len(n_grid)),n_grid)
 plt.yticks(np.arange(0,max_k+1),np.arange(0,max_k+1))
 plt.xlabel("Sample size n")
 plt.ylabel(r"Estimator $\hat{m}$")
+plt.savefig(os.path.join('results',file_name+'_boxplot_hatm'+'.png'))
 plt.show()
 
+
+sns.boxplot(x=df.n,y=df.pred_error)
+plt.xticks(np.arange(len(n_grid)),n_grid)
+plt.xlabel("Sample size n")
+plt.ylabel(r"Prediction error")
+plt.savefig(os.path.join('results',file_name+'_boxplot_pred_errror'+'.png'))
+plt.show()
--- simuObjects.py
+++ simuObjects.py
@@ -9,6 +9,19 @@ from sklearn.linear_model import LassoCV,Lasso
 sns.set()
 
 def get_curve_length(path):
+	'''Computes the length of a picewise linear path
+
+	Parameters
+	----------
+	path: array, shape (n_points,d)
+		The array storing the n_points coordinates in R^d that constitute a 
+		piecewise linear path.
+	
+	Returns
+	-------
+	length: float
+		The length of path.
+	'''
 	length=0
 	for i in range(path.shape[0]-1):
 		length+=math.sqrt(np.sum((path[i+1,:]-path[i,:])**2))
@@ -16,33 +29,130 @@ def get_curve_length(path):
 
 
 def get_signature(path,order):
-	#path=path/get_curve_length(path)
+	''' Returns the signature of a path truncated at a certain order.
+
+	Parameters
+	----------
+	path: array, shape (n_points,d)
+		The array storing the n_points coordinates in R^d that constitute a 
+		piecewise linear path.
+
+	order: int
+		The truncation order of the signature
+
+	Returns
+	-------
+	sig: array, shape (p)
+		Array containing the truncated signature coefficients of path. It is of
+		shape p=(d^(order+1)-1)/(d-1)
+	'''
+
 	return(isig.sig(path,order))
 
 
+def get_SigX(X,k,d):
+	'''Returns a matrix containing signatures truncated at k of n samples
+	given in the input tensor X.
+
+	Parameters
+	----------
+	X: array, shape (n,n_points,d)
+		A 3-dimensional array, containing the coordinates in R^d of n
+		piecewise linear paths, each composed of n_points.
+
+	k: int
+		Truncation order of the signature
+
+	Returns
+	-------
+	SigX: array, shape (n,p)
+		A matrix containing in each row the signature truncated at k of a
+		sample. It is normalized by dividing each column by its absolute
+		value over the n samples, so that every coefficient of SigX is
+		between -1 and 1.
+	'''
+	SigX=np.zeros((np.shape(X)[0],isig.siglength(d,k)))
+	#print("Create signature matrix")
+	for i in range(np.shape(X)[0]):
+		SigX[i,:]=get_signature(X[i,:,:],k)
+	#print("Input matrix size: ",SigX.shape)
+	max_SigX=np.amax(np.absolute(SigX),axis=0)
+	#SigX=SigX/max_SigX
+	return(SigX)
+
 class orderEstimator(object):
+	''' Object that implements the estimation of the truncation order.
+
+	Parameters
+	----------
+	d: int
+		Dimension of the space of the paths X, from which an output Y is
+		learned.
+	
+	rho: float
+		Parameter of the penalization: power of 1/n. It should satisfy :
+		0<rho<1/2.
+	'''
 	def __init__(self,d,rho=1/4):
 		self.rho=rho
 		self.d=d
 
 	def get_penalization(self,n,k,Kpen=1):
+		'''Returns the penalization function used in the estimator of the
+		truncation order, that is, 
+		pen_n(k)=Kpen*sqrt((d^(k+1)-1)/(d-1))/n^rho.
+
+		Parameters
+		----------
+		n: int
+			Number of samples.
+
+		k: int
+			Truncation order of the signature.
+
+		Kpen: float, default=1
+			Constant in front of the penalization, it has to be a positive
+			number.
+
+		Returns
+		-------
+		pen_n(k):float
+			The penalization pen_n(k)
+		'''
 		return(Kpen*n**(-self.rho)*math.sqrt(isig.siglength(self.d,k)))
 
-	def get_SigX(self,X,k):
-		SigX=np.zeros((np.shape(X)[0],isig.siglength(self.d,k)))
-		print("Create signature matrix")
-		for i in range(np.shape(X)[0]):
-			SigX[i,:]=get_signature(X[i,:,:],k)
-		print("Input matrix size: ",SigX.shape)
-		max_SigX=np.amax(np.absolute(SigX),axis=0)
-		SigX=SigX/max_SigX
-		return(SigX)
 
+	def get_alpha_ridgeCV(
+			self,Y,X,k,plot=False,alphas=np.linspace(10**(-6),10,num=1000)):
+		'''Gets the best regularization parameter for a Ridge regression on
+		signatures truncated at k, by scikit-learn built-in cross-validation.
 
-	def get_alpha_ridgeCV(self,Y,X,k,plot=False,alphas=np.linspace(10**(-6),10,num=1000)):
-		#reg=LassoCV()
+		Parameters
+		----------
+		Y: array, shape (n)
+			Array of target values.
+
+		X: array, shape (n,n_points,d)
+			Array of training paths. It is a 3-dimensional array, containing
+			the coordinates in R^d of n piecewise linear paths, each composed of
+			n_points.
+
+		k: int
+			Truncation order of the signature
+
+		plot: boolean, default=False
+			If True, plots the errors against the regularization parameters.
+
+		alphas: array, default=np.linspace(10**(-6),10,num=1000)
+			Values of the regularization paramete to test.
+
+		Returns
+		-------
+		alpha: float
+			The best regularization parameter among alphas.
+		'''
 		reg=RidgeCV(alphas=alphas,store_cv_values=True)
-		SigX=self.get_SigX(X,k)
+		SigX=get_SigX(X,k,self.d)
 		reg.fit(SigX,Y)
 		print("alpha ridge cv: ",reg.alpha_)
 		if plot:
@@ -52,11 +162,43 @@ class orderEstimator(object):
 
 
 	def get_hatL(self,Y,X,k,alpha=1,plot=False):
+		'''Computes the minimum empirical squared loss obtained with a Ridge
+		regression on signatures truncated at k.
+
+		Parameters
+		----------
+		Y: array, shape (n)
+			Array of target values.
+
+		X: array, shape (n,n_points,d)
+			Array of training paths. It is a 3-dimensional array, containing
+			the coordinates in R^d of n piecewise linear paths, each composed of
+			n_points.
+
+		k: int
+			Truncation order of the signature
+
+		alpha: float, default=1
+			Regularization parameter in the Ridge regression.
+
+		plot: boolean, default=False
+			If True, plots the regression coefficients and a scatter plot of the
+			target values Y against its predicted values Ypred to assess the
+			quality
+			of the fit.
+
+		Returns
+		-------
+		hatL: float
+			The squared loss, that is the sum of the squares of Y-Ypred, where
+			Ypred are the fitted values of the Ridge regression of Y against
+			signatures of X truncated at k.
+		'''
 		reg=Ridge(alpha=alpha,normalize=False)
-		SigX=self.get_SigX(X,k)
-		print("Fit ridge regression")
+		SigX=get_SigX(X,k,self.d)
+		#print("Fit ridge regression")
 		reg.fit(SigX,Y)
-		print("End fitting")
+		#print("End fitting")
 		Ypred=reg.predict(SigX)
 		if plot:
 			plt.plot(reg.coef_)
@@ -67,7 +209,52 @@ class orderEstimator(object):
 			plt.show()
 		return(np.sum((Y-Ypred)**2)/len(Y))
 
+	def fit_ridge(self,Y,X,k,alpha=1):
+		reg=Ridge(alpha=alpha,normalize=False)
+		SigX=get_SigX(X,k,self.d)
+		#print("Fit ridge regression")
+		reg.fit(SigX,Y)
+		return(reg)
+
+
 	def get_hatm(self,Y,X,max_k,Kpen=1,alpha=1,plot=False):
+		'''Computes the estimator of the truncation order by minimizing the sum
+		of hatL and the penalization, over values of k from 1 to max_k.
+
+		Parameters
+		----------
+		Y: array, shape (n)
+			Array of target values.
+
+		X: array, shape (n,n_points,d)
+			Array of training paths. It is a 3-dimensional array, containing
+			the coordinates in R^d of n piecewise linear paths, each composed of
+			n_points.
+
+		max_k: int,
+			Maximal value of truncation order to consider. It should be large
+			enough so that the function k -> hatL(k)+penalization(k) does not
+			decrease anymore.
+
+		Kpen: float, default=1
+			Constant in front of the penalization, it has to be a positive
+			number.
+
+		alpha: float, default=1
+			Regularization parameter in the Ridge regression.
+
+		plot: boolean, default=False
+			If True, plots the functions k->hatL(k), k->pen(k) and k-> hatL
+			(k)+pen(k). The latter is minimized at hatm.
+
+		Returns
+		-------
+		hatm: int
+			The estimator of the truncation order
+
+		objective: array, shape (max_k)
+			The array of values of the objective function, minimized at hatm.
+		'''
 		objective=np.zeros(max_k)
 		loss=np.zeros(max_k)
 		pen=np.zeros(max_k)
@@ -85,30 +272,63 @@ class orderEstimator(object):
 			plt.show()
 		return(hatm,objective)
 
-	def slope_heuristic(self,K_values,X,Y,max_k,alpha):
-		hatm=np.zeros(len(K_values))
+	def slope_heuristic(self,Kpen_values,X,Y,max_k,alpha):
+		'''Implements the slope heuristic to select a value for Kpen, the
+		unknown constant in front of the penalization. To this end, hatm is
+		computed for several values of Kpen, and these values are then plotted
+		against Kpen.
+
+		Parameters
+		----------
+		Kpen_values: array, shape (n_K)
+			An array of potential values for Kpen. It must be calibrated so that
+			any value of hatm between 1 and max_k is obtained with values of
+			Kpen in Kpen_values.
+
+		X: array, shape (n,n_points,d)
+			Array of training paths. It is a 3-dimensional array, containing
+			the coordinates in R^d of n piecewise linear paths, each composed of
+			n_points.
+
+		Y: array, shape (n)
+			Array of target values.
+
+		max_k: int,
+			Maximal value of truncation order to consider. It should be large
+			enough so that the function k -> hatL(k)+penalization(k) does not
+			decrease anymore.
+
+		alpha: float, default=1
+			Regularization parameter in the Ridge regression.
+
+		Returns
+		-------
+		hatm: array, shape (n_K)
+			The estimator hatm obtained for each value of Kpen in Kpen_values.
+		'''
+		hatm=np.zeros(len(Kpen_values))
 		loss=np.zeros(max_k)
 		for j in range(max_k):
 			loss[j]=self.get_hatL(Y,X,j+1,alpha=alpha)
 
-		for i in range(len(K_values)):
+		for i in range(len(Kpen_values)):
 			#print(i)
 			pen=np.zeros(max_k)
 			for j in range(max_k):
-				pen[j]=self.get_penalization(Y.shape[0],j+1,Kpen=K_values[i])
+				pen[j]=self.get_penalization(Y.shape[0],j+1,Kpen=Kpen_values[i])
 			hatm[i]=np.argmin(loss+pen)+1
 			#print("Hatm selected: ",hatm[i])
 
 		print(hatm)
-		print(K_values)
+		print(Kpen_values)
 
 		# Plot
 		fig, ax = plt.subplots()
 		jump=1
 		for i in range(1,max_k+1):
 			if i in hatm:
-				xmin=K_values[hatm==i][0]
-				xmax=K_values[hatm==i][-1]
+				xmin=Kpen_values[hatm==i][0]
+				xmax=Kpen_values[hatm==i][-1]
 				ax.hlines(i,xmin,xmax,colors='b')
 				if i!=1:
 					ax.vlines(xmax,i,i-jump,linestyles='dashed',colors='b')
@@ -122,50 +342,103 @@ class orderEstimator(object):
 
 		
 class dataSimu(object):
-	def __init__(self,nb_points,nb_simu,d):
-		self.nb_points=nb_points
-		self.nb_simu=nb_simu
-		self.d=d
+	'''Object that simulated some training data.
 
-	def get_X(self):
-		X=np.zeros((self.nb_simu,self.nb_points,self.d))
-		Xlength=np.zeros((self.nb_simu,self.nb_points,self.d))
-		times=np.linspace(0,1,num=self.nb_points)
-		for i in range(self.nb_simu):
+	Parameters
+	----------
+	n_points: int
+		Number of points in the piecewise linear approximations.
+
+	n: int
+		Number of samples to simulate.
+
+	d: int
+		Dimension of the output space of the training paths.
+	'''
+	def __init__(self,n_points,d,mast):
+		self.n_points=n_points
+		self.d=d
+		self.mast=mast
+		size_sig=isig.siglength(self.d,self.mast)
+		self.beta=np.exp(np.arange(size_sig)/size_sig)*np.random.random(
+			size=size_sig)
+
+
+	def get_X(self,n):
+		'''Generates n sample paths X:[0,1] -> R^d, defined by
+		X_t=alpha_1 + 10*alpha_2*sinus(2*pi*t/alpha_3) + 10*(t-alpha_4)^3,
+		where the alphas are sampled uniformly over [0,1].X is interpolated by 
+		a piecewise linear function with n_points. Each sample X is normalized
+		so that it has length 1.
+
+		Returns
+		-------
+		X: array, shape (n,n_points,d)
+			Array of training paths. It is a 3-dimensional array, containing
+			the coordinates in R^d of n piecewise linear paths, each composed of
+			n_points.
+		'''
+		X=np.zeros((n,self.n_points,self.d))
+		Xlength=np.zeros((n,self.n_points,self.d))
+		times=np.linspace(0,1,num=self.n_points)
+		for i in range(n):
 			for j in range(self.d-1):
 				param=np.random.random(size=4)
-				X[i,:,j]=param[0]+ 10*param[1]*np.sin(times*np.pi*2/param[2]) + 10*(times-param[3])**3
+				X[i,:,j]=param[0]+ 10*param[1]*np.sin(
+					times*np.pi*2/param[2])+ 10*(times-param[3])**3
 			X[i,:,self.d-1]=times
-			Xlength[i,:,:]=np.full((self.nb_points,self.d),get_curve_length(X[i,:,:]))
+			Xlength[i,:,:]=np.full((self.n_points,self.d),get_curve_length(X[i,:,:]))
 		#plt.plot(X[0,:,:])
 		#plt.show()
 		return(X/Xlength)
 
-	def get_Y_sig(self,X,order,noise_std,plot=False):
-		Y=np.zeros(self.nb_simu)
-		noise=np.random.normal(scale=noise_std,size=self.nb_simu)
-		size_sig=isig.siglength(self.d,order)
+	def get_Y_sig(self,X,noise_std,plot=False):
+		'''Compute the target values Y as scalar products of the truncated
+		signatures of rows of X with a certain parameter beta plus a gaussian
+		noise. Y follows therefore the expected signature model.
+		
+		Parameters
+		----------
+		X: array, shape (n,n_points,d)
+			Array of training paths. It is a 3-dimensional array, containing
+			the coordinates in R^d of n piecewise linear paths, each composed of
+			n_points.
 
-		SigX=np.zeros((self.nb_simu,size_sig))
+		noise_std: float
+			Standard error of the Gaussian noise.
 
-		for i in range(self.nb_simu):
-			SigX[i,:]=get_signature(X[i,:,:],order)
+		plot: boolean, default=False
+			If True, output two plots: one plot with the signature coefficients
+			ofone sample and the regression vector beta, one scatter plot with Y
+			against Y+noise to check the calibration of the noise variance.
 
-		#beta=np.exp(np.linspace(0,10,num=size_sig))
-		#beta=np.full(size_sig,1)
-		beta=np.random.random(size=size_sig)
-		beta=beta/math.sqrt(np.sum(beta**2))
+		Returns
+		-------
+		Y: array, shape (n)
+			Target values, defined by the expected signature model.
+		'''
+		n=X.shape[0]
+		Y=np.zeros(n)
+		noise=np.random.normal(scale=noise_std,size=n)
+		size_sig=isig.siglength(self.d,self.mast)
 
-		beta_repeated=np.repeat(beta.reshape(1,size_sig),self.nb_simu,axis=0)
+		SigX=get_SigX(X,self.mast,self.d)
+		#beta=beta/math.sqrt(np.sum(beta**2))
+
+		beta_repeated=np.repeat(self.beta.reshape(1,size_sig),n,axis=0)
 
 		#Y=np.sum(X[:,:,0],axis=1)
 		#Y=Y/math.sqrt(np.sum(Y**2))
 		#print(np.shape(Y))
 		Y=1000*np.sum(beta_repeated*SigX,axis=1)
 		if plot:
-			plt.plot(SigX[0,:])
-			plt.plot(beta)
+			plt.plot(SigX[0,:],label="Signature coefficients")
+			plt.plot(SigX[1,:],label="Signature coefficients")
+			plt.plot(self.beta,label="beta")
 			plt.title("SigX and beta")
+			plt.legend()
+			plt.show()
+			plt.plot(SigX[0,:]*self.beta,label="product")
 			plt.show()
 			plt.scatter(Y,Y+noise)
 			plt.title("Y against Y+noise")
@@ -173,16 +446,14 @@ class dataSimu(object):
 		return(Y+noise)
 
 	def get_Y_nonlinear(self,X,noise_std):
-		Y=np.zeros(self.nb_simu)
-		for i in range(self.nb_simu):
+		n=X.shape[0]
+		Y=np.zeros(n)
+		for i in range(n):
 			Y[i]+=np.mean(np.prod(X[i,:,:],axis=1))*10000
-		noise=np.random.normal(scale=noise_std,size=self.nb_simu)
+		noise=np.random.normal(scale=noise_std,size=n)
 		#plt.scatter(Y,Y+noise)
 		#plt.show()
 		return(Y+noise)
-
-
-
 
 
 
